d73bb4585911489ea6404eab5289d9777a6e8436
Davidpardo15 <101581842+Davidpardo15@users.noreply.github.com>
2022-03-14 11:13:04 -0500

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,19 @@
+function Calculadora(){
+    var x = document.getElementById("precio").value;
+    document.getElementById("cal").innerHTML = x;
+    if (x<=9999 || x>1000000){
+        alert("valor ingresado no valido")
+    }else{
+        var ele = document.getElementsByName('apuesta');           
+        for(i = 0; i < ele.length; i++) {
+            if(ele[i].checked){
+                var result = ele[i].value*x
+                var ga =document.getElementById("gana")
+                ga.value="$"+result
+    
+                console.log(result)
+            }
+        }
+    }
+    
+}
